ab7b4dd076652324c0c9aca7defc5dc04cd7618a
Julka Kubisa <79585810+juliakubisa@users.noreply.github.com>
2024-03-11 19:02:16 +0100

    add or delete card

--- src/web-api/main.py
+++ src/web-api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import *
 from src.application.card import Card
 from src.application.sql_database import db
 
@@ -14,7 +14,20 @@ def return_cards():
     return jsonify(all_cards)
 
 
-# @app.route("/card/<id>", methods = ['DELETE'])
-# def delete_card():
-#     database.delete_card(id)
-#     return
+@app.route("/card/<card_id>", methods = ['DELETE'])
+def delete_card(card_id):
+    card_to_delete = Card.query.get_or_404(card_id)
+    db.session.delete(card_to_delete)
+    db.session.commit()
+    return "Card deleted", 200
+
+@app.route("/card", methods= ['POST'])
+def add_card():
+    if request.method == 'POST':
+        body = request.get_json()
+        foreign_word = body['foreign_word']
+        translated_word = body['translated_word']
+        new_card = Card(translated_word, foreign_word)
+        db.session.add(new_card)
+        db.session.commit()
+        return "Card sucesfully added", 200
