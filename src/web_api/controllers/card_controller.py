1ea43272ef696ab0121dd0cd73eaa284a7d5a497
Julka Kubisa <79585810+juliakubisa@users.noreply.github.com>
2024-03-28 15:57:13 +0100

    edit card doubled word validation

--- src/web_api/controllers/card_controller.py
+++ src/web_api/controllers/card_controller.py
@@ -1,6 +1,6 @@
 from flask import *
 from io import StringIO
-from sqlalchemy import exists
+from sqlalchemy import exists, not_
 from src.application.card import Card
 from src.application.sql_database import db
 from src.application.utils import allowed_file_extension, max_dict_value_len
@@ -8,26 +8,28 @@ from src.application.input import read_input_file
 
 card_controller = Blueprint('card_controller', __name__)
 
+
 @card_controller.route("/cards", methods=['GET'])
 def return_cards():
     all_cards = db.session.query(Card).all()
     return jsonify(all_cards)
 
 
-@card_controller.route("/card/<card_id>", methods = ['DELETE'])
+@card_controller.route("/card/<card_id>", methods=['DELETE'])
 def delete_card(card_id):
     card_to_delete = Card.query.get(card_id)
     db.session.delete(card_to_delete)
     db.session.commit()
     return "Card deleted", 200
 
-@card_controller.route("/card", methods= ['POST'])
+
+@card_controller.route("/card", methods=['POST'])
 def add_card():
     body = request.get_json()
     max_foreign_len = max_dict_value_len(100, "foreign_word", body)
     max_transl_len = max_dict_value_len(100, "translated_word", body)
-    does_card_exist = db.session.query(exists().where(Card.foreign_word == body['foreign_word']).
-                           where(Card.translated_word == body['translated_word'])).scalar()
+    does_card_exist = db.session.query(exists().where(Card.foreign_word == body['foreign_word'])
+                                       .where(Card.translated_word == body['translated_word'])).scalar()
     if (does_card_exist is False and body['foreign_word'] and body['translated_word']
             and max_foreign_len and max_transl_len):
         new_card = Card(body['foreign_word'], body['translated_word'])
@@ -46,12 +48,14 @@ def add_card():
         return "The fields cannot be empty", 400
 
 
-@card_controller.route("/card/<card_id>", methods=['PUT'])
+@card_controller.route("/card/<card_id>", methods=['PUT']) # TODO: length validation
 def edit_card(card_id):
     body = request.get_json()
-    does_card_exist = db.session.query(exists().where(Card.foreign_word == body['foreign_word']).
-                                       where(Card.translated_word == body['translated_word'])).scalar()
-    if does_card_exist == False:
+    does_card_exist = db.session.query(exists()
+                                       .where(Card.foreign_word == body['foreign_word'])
+                                       .where(Card.translated_word == body['translated_word'])
+                                       .where(not_(Card.id == body['id']))).scalar()
+    if not does_card_exist:
         card_to_edit = Card.query.get(card_id)
         card_to_edit.foreign_word = body['foreign_word']
         card_to_edit.translated_word = body['translated_word']
