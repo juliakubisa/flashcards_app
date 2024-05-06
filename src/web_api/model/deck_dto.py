b4ede2313dde091db6ab7e4f44e82736bd7ddf94
jerzybalcer <jurekbalcer@gmail.com>
2024-05-06 16:38:21 +0200

    deck dto adjustments

--- src/web_api/model/deck_dto.py
+++ src/web_api/model/deck_dto.py
@@ -6,5 +6,6 @@ class DeckDTO:
     def __init__(self, deck):
         self.id = deck.id
         self.name = deck.name
-        self.language = deck.language_id
+        self.language_name = deck.language.name
+        self.language_id = deck.language_id
         self.cards_count = len(deck.cards)
