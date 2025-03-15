f7b505705e765189321075d7b186e6c3249dcbe9
jerzybalcer <jurekbalcer@gmail.com>
2025-03-15 18:34:05 +0100

    Reapply "login with google (prototype)"
    This reverts commit 351650419ba21871d134f6e2bfd2fed89bb08060.

--- src/model/account.py
+++ src/model/account.py
@@ -4,6 +4,7 @@ from src.application.sql_database import db
 
 class Account(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    google_id: Mapped[str] = mapped_column(nullable=True)
     name: Mapped[str] = mapped_column()
     email: Mapped[str] = mapped_column()
     password: Mapped[str] = mapped_column()
